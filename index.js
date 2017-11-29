8d01f4bba391e29b4d211bcf9f0ae50794f876fc
Ashton Reed <dreamergirl411@gmail.com>
2017-11-29 17:13:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-describe('index.js', function () {
-  describe('scuberGreetingForFeet()', function () {
-    it('gives customers a free sample if the ride is less than or equal to 400 feet', function () {
-      expect(scuberGreetingForFeet(199)).to.equal('This one is on me!');
-    });
-
-    it('charges 30 dollars for a distance over 2000 feet', function () {
-      expect(scuberGreetingForFeet(2001)).to.equal('I will gladly take your thirty bucks.');
-    });
-
-    it('does not allow rides over 2500 feet', function () {
-      expect(scuberGreetingForFeet(2501)).to.equal('No can do.');
-    });
-  });
-
   function scuberGreetingForFeet (distance) {
     if (distance <= 400) {
       return ('This one is on me!')
